f55ffcd8aede371137da26dec0fa899c86cbe4ee
k.masik <k.masik@2gis.ru>
2018-04-11 19:11:05 +0700

    Day 1 - Push to master
    !!!

---
+++ RaidLeaderToolkit/RaidLeaderToolkit/RaidLeaderToolKit.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RaidLeaderToolkit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var json = new WebClient().DownloadString("Some url");
+            
+            TBV.Text = json;
+        }
+    }
+}
